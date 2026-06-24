1895068e66b737f33064c846d56e79f68b5792bc
andrewiankidd <contact@andrewiankidd.co.uk>
2026-06-24 17:06:45 +0000

    Deploying to gh-pages from @ andrewiankidd/legacy-lua-platformer@0f1980bc131ded032a1f27353e6bbaf94268f6be 🚀

--- Web/game.js
+++ Web/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"2cc39d86-edab-43ce-8ebb-94669dec51b5","remote_package_size":6867997,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":6867997,"audio":false}]});
+  loadPackage({"package_uuid":"9a8f42b3-2417-4488-b42a-9672b56b17e8","remote_package_size":6867971,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":6867971,"audio":false}]});
 
 })();
